de9469c13d59632206a661d4d26fcaff9ed52228
ye ping <ye ping>
2019-05-01 00:18:38 +0800

    test

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="fristcomponent">
-        <h1>I an a title.</h1>
+        <h1>title.</h1>
         <a>write by {{author}}</a>
         <a href="/second">fristcomponent 跳转到 second</a>
     </div>
